fbd9f29ef152738a29f2e5ea46865ed1702e21a0
yi-jin <dlwls4338@naver.com>
2022-09-25 15:24:14 +0900

    Initial commit

---
+++ src/main/java/com/example/calculator/CalculatorApplication.java
@@ -0,0 +1,17 @@
+package com.example.calculator;
+
+
+public class CalculatorApplication {
+
+    public static void main(String[] args) {
+        System.out.println("junit");
+
+
+
+        //Calculator calculator = new Calculator(new KrwCalculator());
+
+       // System.out.println(calculator.sum(10, 10));
+
+    }
+
+}
